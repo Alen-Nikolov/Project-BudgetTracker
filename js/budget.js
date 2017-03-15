ad65b41b8f41a1e6d7811f492b2bbd6316722bbe
Alen-Nikolov <alennikolov95@gmail.com>
2017-03-15 13:00:04 +0200

    Budget.js fixed

--- js/budget.js
+++ js/budget.js
@@ -1,15 +1,22 @@
 (function($) {
     "use strict"; // Start of use strict
+      // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
     });
+        // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -18,19 +25,9 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+  
 
 
-    // Offset for Main Navigation
-    // $('#mainNav').affix({
-    //     offset: {
-    //         top: 100
-    //     }
-    // })
 
 })(jQuery); // End of use strict
 
